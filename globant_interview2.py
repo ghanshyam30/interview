4b4a108f1bd6a89b7478dba9041499464904cc79
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-11-26 12:15:35 +0530

    globant interview2 questions added

---
+++ globant_interview2.py
@@ -0,0 +1,142 @@
+String[] dateArray = {"20/Jun/2020", "01/Apr/2020", "18/Dec/2019"}
+output : Apr Jun Dec
+
+# split_array = []
+# for element in dateArray:
+# 	split_elemt = element.split("/")    # ["20","Jan","2020"]
+# 	split_array.append(split_ele)      # split_array = [["20","Jan","2020"],----]
+
+# months = []
+# for split_item in split_array:
+  # months_item = split_item[1] for each item in split array     # ["Jan", ----]
+  # months.append(months_item)      # month = ["Jan", --]
+
+# months.sort()     ["Apr", "Jun", "Dec"]
+===================================================
+
+
+arr = [1,2,4,3,9,7,11,3,7,11]
+
+max_element = max(arr)
+
+count_max = arr.count(max_element)
+
+====================================================
+WAP to find max consecutive 1’s [0,0,1,1,1,0,0,1,0,1,1,1,1,1,1,0,0,0,1,1,1]
+list1 =[0,0,1,1,1,0,0,1,0,1,1,1,1,1,1,0,0,0,1,1,1]
+
+count_one = 0
+for item in list1:
+    if item == 1:
+    		count_one += 1
+        index_list.append(count_one)     # [2,3,4,---]
+
+max_count =0
+for item in index_list:
+    if item+
+
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+
