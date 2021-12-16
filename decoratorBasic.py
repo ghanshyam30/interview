e406f1a4e13e97733b9eb5c0e5b73766b07ca5ef
Ghansham S Bambale <GS-1766@GSLAB.COM>
2021-12-16 07:45:05 +0530

    formatting of variable names to be consistent

--- decoratorBasic.py
+++ decoratorBasic.py
@@ -3,8 +3,8 @@ def modify_salary(basic):
     
     def calculateDA(turn2):
         da = 0
-        basic_sal = turn2()
-        da = basic_sal * 0.03
+        basic_sal = turn2(10000)
+        da = basic_sal * 0.3
         print("DA:",da)
         return da+basic_sal
     
@@ -15,10 +15,21 @@ def modify_salary(basic):
 
 @modify_salary
 # Original fucntion
-def base_salary():
+def base_salary(total_due):
+    basic = total_due / 2
     print("Base salary is 10 rs")
-    return 10
+    return basic
 
-# base_salary = modify_salary(base_salary)
-store_final= base_salary
+# store_final = modify_salary(base_salary)    #without using @decorator execution
+
+# How to call a function which has wrapper/decorator
+# You don't need to provide arguments here like base_salary(10000) or it will complain that the result is float and is not callable
+store_final= base_salary     
 print("Final: ",store_final)
+
+
+''' OUTPUT
+Base salary is 10 rs
+DA: 1500.0
+Final:  6500.0
+'''
